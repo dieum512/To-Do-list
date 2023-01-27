b0a01645bd1faad5648ff114ca14701393f655ad
dieum512 <akonkwadieum222@gmail.com>
2023-01-27 11:21:28 +0200

    Add the add and remove event

--- src/index.js
+++ src/index.js
@@ -1,40 +1,97 @@
 // import _ from 'lodash';
 import './style.css';
 
-const todos = [
-  {
-    index: 1,
-    description: 'Go to the GYM',
-    completed: true,
-  },
-  {
-    index: 2,
-    description: 'Get breakfast',
-    completed: true,
-  },
-  {
-    index: 3,
-    description: 'Start programming',
-    completed: false,
-  },
-  {
-    index: 4,
-    description: 'Go to bed',
-    completed: false,
-  },
-];
-
-todos.sort((a, b) => a.index - b.index);
-
+// select the form element
+const form = document.querySelector('.first-form');
+const todoInput = document.querySelector('.add-task');
 const todoList = document.querySelector('.to-do-list');
 
-for (let i = 0; i < todos.length; i += 1) {
-  todoList.innerHTML += `
+// variables
+let listTodos = JSON.parse(localStorage.getItem('listTodos')) || [];
+
+// FIRST RENDRER
+renderTodo();
+
+// submit form 
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  addTodo();
+  renderTodo();
+  localStorage.setItem('listTodos', JSON.stringify(listTodos));
+  // RESET FORM
+  form.reset();
+});
+
+// add a to do
+function addTodo() {
+  const todoValue = todoInput.value;
+
+  // check if empty to do 
+  const isEmpty = todoValue === '';
+
+  // CHEK FOR DUPLICATE TO DO
+  const isDupplicated = listTodos.some((todo) => todo.description.toUpperCase() === todoValue.toUpperCase());
+
+  if (isEmpty) {
+    alert(`The to do's task is empty`);
+  } else if (isDupplicated) {
+    alert(`The to do task already exist`);
+  } else {
+    const todo = {
+      description: todoValue,
+      completed : false,
+    }
+  
+    listTodos.push(todo);
+  }
+}
+
+// RENDER TODO
+function renderTodo() {
+  // CLEAR ELEMENT BEFORE THE RE-RENDER
+  todoList.innerHTML = "";
+
+  // RENDER TODO
+  listTodos.forEach((todo, index) => {
+    todoList.innerHTML += `
   <li>
-    <div class="task-div">
-      <input type="checkbox" />
-      <p>${todos[i].description}</p>
+    <div class="todo" id=${index}>
+      <div class="task-div">
+        <input type="checkbox" />
+        <p>${todo.description}</p>
+      </div>
+      <i class="fa-solid fa-trash" data-action="delete"></i>
     </div>
-    <i class="fa-solid fa-trash"></i>
   </li>`;
+    // todoList.clearFields();
+  })
+}
+
+// ADD EVENT LISTNER FOR ALL TODOS
+todoList.addEventListener('click', (e) => {
+  const target = e.target;
+  const parentElement = target.parentElement;
+
+  if (parentElement.className !== 'todo') return;
+
+  // TO DO ID
+  const todo = parentElement;
+  const todoId = Number(todo.id);
+
+  // TARGET ACTION
+  const action = target.dataset.action;
+
+  action === "delete" && deleteTodo(todoId)
+
+  console.log(todoId, action)
+})
+
+// DELETE TODO
+function deleteTodo(todoId) {
+  listTodos = listTodos.filter((todo, index) => index !== todoId);
+
+  // RE RENDER
+  renderTodo()
+  localStorage.setItem('listTodos', JSON.stringify(listTodos));
 }
